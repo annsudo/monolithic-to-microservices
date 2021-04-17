63c4c079f4377cfa7ac402faeca1c555d8a34d56
Anna <anna.nikolskaya@hotmail.com>
2021-04-17 22:41:58 +0200

    podcast-info-service final

--- finnish/podcast-info-service/src/main/java/com/devops/podcastinfoservice/resources/PodcastResource.java
+++ finnish/podcast-info-service/src/main/java/com/devops/podcastinfoservice/resources/PodcastResource.java
@@ -1,18 +1,24 @@
 package com.devops.podcastinfoservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.devops.podcastinfoservice.models.Podcast;
 
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping ("/podcasts")
 public class PodcastResource {
    
-    @RequestMapping("/{podcastId}")
-    public Podcast getPodcast(@PathVariable("podcastId") String podcastId){
-        return new Podcast(podcastId, "The school of Greatness");
+    @RequestMapping ("/podcasts")
+    public List <Podcast> getPodcast(){
+        return Arrays.asList(
+        new Podcast("1", "The school of Greatness"),
+        new Podcast("2", "Security now"),
+        new Podcast("3", "Frontiers of learning")
+        );
+        
     }
 
 }
