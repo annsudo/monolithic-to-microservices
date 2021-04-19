822235daff9986acd28020e215f056666ac0520e
annsudo <55130711+annsudo@users.noreply.github.com>
2021-04-19 13:53:06 +0200

    All templates for the project
    All templates for the project

--- start-microservices/podcast-catalog-service/src/main/java/com/devops/podcastcatalogservice/resources/PodcastCatalogResource.java
+++ start-microservices/podcast-catalog-service/src/main/java/com/devops/podcastcatalogservice/resources/PodcastCatalogResource.java
@@ -25,7 +25,6 @@ public class PodcastCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
-
     @RequestMapping("/{userId}") 
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
