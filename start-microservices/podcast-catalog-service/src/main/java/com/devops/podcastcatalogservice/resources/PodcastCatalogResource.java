e7c52cdee86d03e3720c2d136dc6983d4c6f7d1d
Anna <anna.nikolskaya@hotmail.com>
2021-04-19 11:08:03 +0200

    1

--- start-microservices/podcast-catalog-service/src/main/java/com/devops/podcastcatalogservice/resources/PodcastCatalogResource.java
+++ start-microservices/podcast-catalog-service/src/main/java/com/devops/podcastcatalogservice/resources/PodcastCatalogResource.java
@@ -14,9 +14,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 
+
+
+@RestController
+@RequestMapping("/catalog")
 public class PodcastCatalogResource {
 
+    @RequestMapping("/{userId}") 
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+
+    }
 }
